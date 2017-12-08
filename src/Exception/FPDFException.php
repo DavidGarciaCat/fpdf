1b172efdb6449269d6555ca633fd37e0fbbb033e
David Garcia <david.garcia@deralia.com>
2017-12-08 22:48:05 +0000

    Apply fixes from StyleCI

--- src/Exception/FPDFException.php
+++ src/Exception/FPDFException.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * FPDF
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
 namespace DavidGarciaCat\FPDF\Exception;
 
 class FPDFException extends \Exception
